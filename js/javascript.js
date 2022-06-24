391d226bcc1cb4716912f766c9c990c8b8269c8a
Anthony LoneFight <tonylonefight@gmail.com>
2022-06-23 21:34:25 -0500

    initial upload

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+
+ var headerE1 = document.querySelector('h1');
+ headerE1.innerText = 'Art slideshow';
+ headerE1.style.color = 'rgb(52, 98, 74)';
+ headerE1.style.marginLeft = '20px';
+
+var artShow1 = 3
+
+console.log(artShow1);
+
+if (artShow1 == 1) {
+var tlfArt = document.getElementById("myArt").src = "assets/coffeenadonut.png";
+}
+
+if (artShow1 == 2) {
+var tlfArt = document.getElementById("myArt").src = "assets/oldman.jpg";
+}
+
+if (artShow1 == 3) {
+var tlfArt = document.getElementById("myArt").src = "assets/bread.jpg";
+}
+
+if (artShow1 == 4) {
+var tlfArt = document.getElementById("myArt").src = "assets/logopng.png";
+}
+
+if (artShow1 == 5) {
+var tlfArt = document.getElementById("myArt").src = "assets/water.jpg";
+}
+
+
